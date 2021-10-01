d10ca7c6c17e8c9c8b6ee9fe684fe5a24c6299cf
AKBAR MOHAMMAD <43230334+AKBARMOHAMMAD@users.noreply.github.com>
2021-10-01 11:51:59 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,2 +1,9 @@
 def call(map stageParams){
-  
+  checkout([
+	$class: 'GitSCM', 
+	branches: [[name: '*/master']], 
+	extensions: [], 
+	userRemoteConfigs: [[url: 'https://github.com/AKBARMOHAMMAD/Sample_maven_project.git']]
+])
+
+}
